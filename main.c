61588aee31d32d8b85602e3bf1fff22b97ecca65
tobiasfriden <tobias.friden@gmail.com>
2017-03-27 12:06:52 +0200

    Update KOM

--- main.c
+++ main.c
@@ -5,22 +5,29 @@
  * Author : Tobias Fridén
  */ 
 
-
+#define F_CPU 14745600UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define BAUD 9600		//Sätter baudrate (bps)
+#define BAUD 115200		//Sätter baudrate (bps)
 #define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)  
 
-#ifndef F_CPU
-#define F_CPU 14745600UL
-#endif
+
 
 //Initialisera UART
 void uart_init(void)
 {
+	//Aktivera UART0
+	UBRR0H = (BAUDRATE>>8);
+	UBRR0L = BAUDRATE;
+	//Aktivera reciever och transmitter
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
+	// Sätter frame format: 8data, 2stop bit
+	UCSR0C = (1<<USBS1)|(3<<UCSZ10);	
+	
+	//Aktivera UART1
 	UBRR1H = (BAUDRATE>>8);
 	UBRR1L = BAUDRATE;
 	//Aktivera reciever och transmitter
@@ -38,7 +45,15 @@ void spi_init(void)
 	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPIE);
 }
 
-void transmit_uart(unsigned char data)
+void transmit_uart0(unsigned char data)
+{
+	//Väntar på att utdatabuffer ska bli tom
+	while ( !( UCSR0A & (1<<UDRE1)) );
+	//Skickar data
+	UDR0 = data;
+}
+
+void transmit_uart1(unsigned char data)
 {
 	//Väntar på att utdatabuffer ska bli tom
 	while ( !( UCSR1A & (1<<UDRE1)) );
@@ -46,7 +61,7 @@ void transmit_uart(unsigned char data)
 	UDR1 = data;
 }
 
-void transmit_spi(char data)
+void transmit_spi(unsigned char data)
 {
 	//Skicka data
 	SPDR = data;
@@ -55,22 +70,40 @@ void transmit_spi(char data)
 }
 
 
-struct indata_buffer{
-		unsigned char uart_indata;
-		unsigned char spi_indata;
-	};
+volatile unsigned char uart0_indata = 0x00;
+volatile unsigned char uart1_indata = 0x00;
+volatile unsigned char spi_indata = 0x00;
 
-
-int main(void)
-{
 	
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
 
 ISR(SPI_STC_vect)
 {
 	spi_indata=SPDR;
 }
+
+ISR(USART0_RX_vect)
+{
+	uart0_indata=UDR0;
+}
+
+ISR(USART1_RX_vect)
+{
+	uart1_indata=UDR1;
+}
+
+
+
+int main(void)
+{
+	sei();
+	uart_init();
+	spi_init();
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		transmit_uart0(uart0_indata);
+		_delay_ms(500);
+    }
+}
+
