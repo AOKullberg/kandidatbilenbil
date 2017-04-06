cc8396d71d37e074c211c16e5e172150c3c2b042
tobiasfriden <tobias.friden@gmail.com>
2017-04-06 15:01:37 +0000

    Update ultraljud.cpp

--- ultraljud.cpp
+++ ultraljud.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -9,10 +11,19 @@ using namespace std;
 /* FUNCTION ping_ultra_myseconds()
  * pingar sensorn och returnerar svarstiden i mikrosekunder
  */
-int ping_ultra_myseconds(int outputpin, int inputpin)
+vector<int> ping_ultra_myseconds()
 {
-	int mysec=0;
+
+	int mysec_back=0;
+	int mysec_right=0;
+	int mysec_left=0;
 	
+	int outputpin = 1;
+	int input_back=22;
+	int input_right=23;
+	int input_left=21;
+	
+	vector<int> data{0};
 
 	//Gör sensorn redo för ping
 	digitalWrite(outputpin,HIGH);
@@ -23,42 +34,92 @@ int ping_ultra_myseconds(int outputpin, int inputpin)
 	digitalWrite(outputpin,LOW);
 	
 	//Väntar på positiv flank
-	while(! digitalRead(inputpin))
+	while(! digitalRead(input_back))
 	{
-		//cout << "Loop1 \n";
 		delayMicroseconds(1);
 	}
 	
+	while( digitalRead(input_back) || digitalRead(input_right) 
+			|| digitalRead(input_left)){
+				
+				if( digitalRead(input_back) ){
+					++mysec_back;
+				}
+				
+				if( digitalRead(input_right) ){
+					++mysec_right;
+				}
+				
+				if( digitalRead(input_left) ){
+					++mysec_left;
+				}
+				
+				delayMicroseconds(1);
+			}
+	
+	/*
 	//Räknar fram tills negativ flank
 	while( digitalRead(inputpin))
 	{
-		//cout << "Loop2 \n";
 		++mysec;
 		delayMicroseconds(1);
-	}
+	}*/
+	
+	data[0]=mysec_back;
+	data[1]=mysec_right;
+	data[2]=mysec_left;
+	
 	delay(50);
 
-	return mysec;
+	return data;
 }
 
 /* FUNCTION ping_ultra_distance()
  * pingar sensorn 5 gånger, räknar ut medelvärde och
  * returnerar avstånd i mikrometer
  */
-int ping_ultra_distance(int outputpin, int inputpin)
+vector<int> ping_ultra_distance()
 {
-	int time = 0;
+	
+	vector <int> back{0};
+	vector <int> right{0};
+	vector <int> left{0};
+	
+	vector <int> temp{0};
+	vector <int> data{0};
+	
+	
 	for(int i=0;i<5;i++)
 	{
-		time += ping_ultra_myseconds(outputpin, inputpin);
+		temp = ping_ultra_myseconds();
+		back.push_back(temp[0]);
+		right.push_back(temp[1]);
+		left.push_back(temp[2]);
 	}
+	
+	sort(back.begin(), back.end());
+	sort(right.begin(), right.end());
+	sort(left.begin(), left.end());
+	
+	data.push_back(back[3]*340/10000);
+	data.push_back(right[3]*340/10000);
+	data.push_back(left[3]*340/10000);
+	/*
+	for( int i : data){
+		cout << i << endl;
+	}*/
 
-	return (time/5)*340;
+	return data;
 }
-
+/* FUNCTION ultraljud_setup()
+ * Ställer in de inställningar som krävs för ultraljudssensorer
+ */
 void ultraljud_setup(){
-	pinMode (1, OUTPUT);
+	pinMode (1, OUTPUT); 
 	pinMode (21, INPUT);
+	pinMode (22, INPUT);
+	pinMode (23, INPUT); 
+	
 }
 	
 
