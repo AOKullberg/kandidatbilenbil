8e8fdfb06241063fc6e4fd4318f35062a5fcf7be
Carlman9 <carra862@student.liu.se>
2017-04-12 15:42:34 +0200

    gardsplan update

--- gardsplan.c
+++ gardsplan.c
@@ -10,6 +10,7 @@
  #include "main.h"
  #include "motorstyrning.h"
  #include <avr/io.h>
+ #include <util/delay.h>
 
 
 
@@ -17,16 +18,16 @@ void back_out_from_garage(void)
 {
   while(distance_forward < 45)
   {
-    retardate(2);
+    retardate(1);
   }
-  retardate(1);
 }
 
 
 
 void first_right_turn(void)
 {
-  uint8_t HighData;
+
+  /*uint8_t HighData;
 	uint8_t LowData;
 	float Angle = 0;
   SetUpIMU();
@@ -35,14 +36,18 @@ void first_right_turn(void)
   {
     GetI2CData(AngularVelocitySlaveAddress, AngularVelocity_OUT_Z_L_Register, &LowData);	//Hämta låg och hög databyte
   	GetI2CData(AngularVelocitySlaveAddress, AngularVelocity_OUT_Z_H_Register, &HighData);
-//minus framför angle eftersom att det är en högersväng så gyrot kommer ge -grader, lättare att göra om till pos.
-    Angle -= ConvertToAngles(((uint16_t)HighData << 8) | LowData);	//Lägger till skillnaden i vinkel varje loop
+    Angle += ConvertToAngles(((uint16_t)HighData << 8) | LowData);	//Lägger till skillnaden i vinkel varje loop
     turn_right(5);
-    accelerate_2(1);
+  }*/
+  turn_right(5);
 
-
-  }
 }
+
+void drive_forward(void)
+{
+  
+}
+
 
 
 
@@ -50,6 +55,14 @@ void first_right_turn(void)
 void gardsplan(void)
 {
   drive_out_from_garage();
+  first_right_turn();
+  for (int i=0; i<177; i++)
+  {
+    _delay_ms(17);
+  }
+  break();
+  turn_left(5);
+
 
 
 
