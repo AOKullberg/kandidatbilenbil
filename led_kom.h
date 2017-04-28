47e361db0e16b5d9b20cf9dbb1851154740f539a
tobiasfriden <tobias.friden@gmail.com>
2017-04-28 09:17:55 +0200

    Add files via upload

---
+++ led_kom.h
@@ -0,0 +1,17 @@
+﻿/*
+ * led_kom.h
+ *
+ * Created: 4/28/2017 9:13:49 AM
+ *  Author: tobfr427
+ */ 
+
+#define F_CPU 14745600UL
+
+#ifndef LED_KOM_H_
+#define LED_KOM_H_
+
+void delay( int mysec );
+void led_init( void );
+void blink_led(int nr, int mysec);
+
+#endif
